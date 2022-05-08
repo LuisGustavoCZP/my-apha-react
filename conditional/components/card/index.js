7a059efcae87e784ffde56f332bb0cebad132fe8
Luis Gustavo Cardoso Zanetti Pereira <49000286+LuisGustavoCZP@users.noreply.github.com>
2022-05-08 19:09:12 -0300

    update: 1.0

--- conditional/components/card/index.js
+++ conditional/components/card/index.js
@@ -1,11 +1,34 @@
 function Card(character) 
 {
-    if(character.status == "Alive")
-        return React.createElement("span", null, character.name);
-    else if(character.status == "Dead")
-        return React.createElement("span", null, character.name, React.createElement("span", {className:"status red"}, "MORTO"));
-    else
-        return React.createElement("span", null, character.name, React.createElement("span", {className:"status gray"}, "DESCONHECIDO"));
+    const status = (character.status == "Alive"?"green":character.status == "Dead"?"red":"gray");
+
+    const parts = [];
+    parts.push(React.createElement("h3", {className:"char-name"}, character.name));
+    const imgParts = [];
+    imgParts.push(React.createElement("img", {src:character.image}));
+    if(status == "gray")
+        imgParts.push(React.createElement("div", {className:`status ${status}`}, React.createElement("h3", null, "DESCONHECIDO")));
+    else if(status == "red")
+        imgParts.push(React.createElement("div", {className:`status ${status}`}, React.createElement("h3", null, "MORTO")));
+
+    parts.push(React.createElement("div", null, imgParts));
+    
+    const specie = character.species=="Human"?"Humano":character.species=="Humanoid"?"Humanoide":character.species=="Robot"?"Robô":character.species=="Mythological Creature"?"Criatura Mitológica":character.species=="Alien"?"Alienigena":character.species=="unknown"?"Desconhecido":character.species;
+    const gender = character.gender=="Female"?"Fêmea":character.gender=="Male"?"Macho":character.gender=="unknown"?"Desconhecido":character.gender=="Genderless"?"Assexuado":character.gender=="Genderless"?"Assexuado":character.gender;
+
+    parts.push(React.createElement("span", {className:"info specime"},
+        React.createElement("h4", null, ``),
+        React.createElement("h4", null, `${specie} ${gender}`)
+    ));
+
+    const origin = character.origin.includes("Replacement Dimension")?character.origin.replace("Replacement Dimension", "Substituta"):character.origin;
+
+    parts.push(React.createElement("span", {className:"info origin"},
+        React.createElement("h5", null, `Origem:`),
+        React.createElement("h4", null, `${origin}`)
+    ));
+
+    return React.createElement("li", {className:`card ${status}`}, parts);
 }
 
 export default Card;
--- conditional/components/home/index.js
+++ conditional/components/home/index.js
@@ -1,44 +1,143 @@
 import Card from "../card/index.js"
 const { useState, useEffect } = React;
 
+let search = "";
 let charList = [];
+
 
 function Home () 
 {
     const [loaded, setLoaded] = useState(false);
-    //const [characters, setCharacters] = useState([]);
 
-    async function loadcards (setLoaded) 
+    useEffect(()=>
     {
-        const resEp = await fetch("https://rickandmortyapi.com/api/episode/1");
-        const respEp = await resEp.json();
-        const charIDs = respEp.characters;
-        //console.log(charIDs);
-        charList = [];
-        for(let i = 0; i < charIDs.length; i++)
+        if(!search)
         {
-            const characterID = charIDs[i];
-            const resChars = await fetch(characterID);
-            const respChars = await resChars.json();
-            const character = React.createElement("li", {key:i, className:"card"}, Card(respChars));
-            charList.push(character);
+            document.getElementById("input-episode").value = "1";
+            loadcards("1")
         }
+    });
+
+    async function loadcards (ep) 
+    {
+        console.log(ep);
+        //const ep = e.target.value;
+        charList = [];
+        setLoaded(false);
+        search = ep;
+        if(ep <= 0) 
+        {
+            search = "";
+            return;
+        }
+
+        const resEp = await fetch(`https://rickandmortyapi.com/api/episode/${ep}`);
+        const respEp = await resEp.json();
+        const charURLs = respEp.characters;
+        console.log(charURLs);
+        const charIDs = [];
+        for(let i = 0; i < charURLs.length; i++)
+        {
+            const characterURL = charURLs[i];
+            //console.log(characterURL)
+            const lasti = characterURL.slice(characterURL.lastIndexOf("/")+1);
+            //console.log(lasti)
+            const id = parseInt(lasti);
+            if(id != NaN) charIDs.push(id);
+        }
+
+        const charsurl = `https://rickandmortyapi.com/api/character/${charIDs}`;
+        console.log(charsurl);
+        const resChars = await fetch(charsurl);
+        const respChars = await resChars.json();
+        
+        const charLocs = {0:[]};
+        for(let i = 0; i < respChars.length; i++)
+        {
+            const locationURL = respChars[i].origin.url;
+            //console.log(locationURL)
+            const lasti = locationURL.slice(locationURL.lastIndexOf("/")+1);
+            if(lasti != "") {
+                const id = parseInt(lasti);
+                let charloc = charLocs[id];
+                if(!charloc) 
+                {
+                    charloc = [];
+                    charLocs[id] = charloc;
+                };
+                charloc.push(i);
+            }
+            else 
+            {
+                charLocs[0].push(i);
+            };
+        }
+
+        console.log(charLocs);
+
+        const locsURL = `https://rickandmortyapi.com/api/location/${Object.keys(charLocs)}`;
+        console.log(locsURL);
+        const locations = locsURL;
+        const resOrigin = await fetch(locations);
+        const respOrigin = await resOrigin.json();
+        
+        for(let i = 0; i < respOrigin.length; i++)
+        {
+            const origin = respOrigin[i];
+            const locChars = charLocs[origin.id];
+            for(let j = 0; j < locChars.length; j++)
+            {
+                const character = respChars[locChars[j]];
+                if(origin.name == "unknown") character.origin = "Desconhecida";
+                else character.origin = origin.name.replace("Earth", "Terra");
+
+                charList.push(Card(character));
+            }
+        }
+       
+        console.log(charList);
         setLoaded(true);
     }
+    
+    const els = [];
 
+    els.push(React.createElement("header", {id:"header", class:"Header"}, 
+        React.createElement("h1", null, "Rick and Morty on React"),
+        React.createElement("span", {id:"episode", class:"episode"},
+            React.createElement("label", {class:"episode", for:"input-episode"}, "Episodio"),
+            React.createElement("input", {id:"input-episode", type:"number", class:"episode", placeholder:"Insira aqui um numero de episódio", onChange:(e) => loadcards(e.target.value)})
+        )
+    ));
     if(!loaded)
     {
-        loadcards (setLoaded);
-        return React.createElement("div", {id:"home", class:"Load"}, "Loading");
+        const texts = [];
+        if(!search)
+            texts.push(React.createElement("h2", {class:"empty"}, "Você pode precisa escolher um episodio!"));
+        else 
+        {
+            texts.push(React.createElement("div", {class:"load"},
+                React.createElement("div", {class:"ring"}, 
+                    React.createElement("span")
+                ),
+                React.createElement("h2", null, 
+                    React.createElement("span", null, "Carregando"), 
+                    React.createElement("span", null, "...")
+                )
+            ));
+        }
+
+        els.push(React.createElement("div", {id:"home", class:"Home"}, texts));
     }
     else
     {
-        return React.createElement(
+        els.push(React.createElement(
             "div", 
             {id:"home", class:"Home"}, 
-            charList
-        );
+            React.createElement("ul", {className:"cards"}, charList)
+        ));
     }
+
+    return els;
 }
 
 export default Home;
